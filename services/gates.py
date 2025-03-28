9d4c29d45ccd5df5f1af26d68dc3e7dea4b58316
Rokas Paulauskas <ifamous.rp@gmail.com>
2025-03-28 17:16:52 +0000

    Fix url for gs

--- services/gates.py
+++ services/gates.py
@@ -5,10 +5,10 @@ def open_gate(number, gate):
     try:
         if gate == "entry":
             response = requests.post(
-                f"http://localhost:8000/api/v1/sessions/entry", json={"plate_number": number})
+                f"http://localhost:8000/gs/api/v1/sessions/entry", json={"plate_number": number})
         elif gate == "exit":
             response = requests.put(
-                f"http://localhost:8000/api/v1/sessions/exit", json={"plate_number": number})
+                f"http://localhost:8000/gs/api/v1/sessions/exit", json={"plate_number": number})
         return response.json()
     except Exception as e:
         raise Exception(f"[GATE] Failed to open gate: {e}")
