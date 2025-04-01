68af2958b97f2aeb540724ee8b666970037c8c75
Rokas Paulauskas <ifamous.rp@gmail.com>
2025-04-01 18:03:30 +0100

    No need for https as it's internal call

--- services/gates.py
+++ services/gates.py
@@ -5,10 +5,10 @@ def open_gate(number, gate):
     try:
         if gate == "entry":
             response = requests.post(
-                f"https://138.199.217.16/gs/api/v1/sessions/entry", json={"plate_number": number})
+                f"http://138.199.217.16/gs/api/v1/sessions/entry", json={"plate_number": number})
         elif gate == "exit":
             response = requests.put(
-                f"https://138.199.217.16/gs/api/v1/sessions/exit", json={"plate_number": number})
+                f"http://138.199.217.16/gs/api/v1/sessions/exit", json={"plate_number": number})
         return response.json()
     except Exception as e:
         raise Exception(f"[GATE] Failed to open gate: {e}")
